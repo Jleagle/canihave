21666d1b142ebee69e86a352e6fa088f4ab77fab
James Eagle <james.eagle@fortifi.it>
2017-10-20 13:24:54 +0100

    Check error is an error

--- logger/logger.go
+++ logger/logger.go
@@ -31,7 +31,7 @@ func log(level logging.Severity, message string, err ...error) {
 
 	message = time.Now().Format("2006-01-02 15-04-05") + " - " + message
 
-	if len(err) > 0 {
+	if len(err) > 0 && err[0] != nil {
 		message = message + ": " + err[0].Error()
 	}
 
