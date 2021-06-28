5354a802e5117224c58d683fa57c4f3b453326d5
James Eagle <jimeagle@gmail.com>
2021-06-28 22:50:44 +0100

    Fix MaxMind auto update script

--- update_geoip.sh
+++ update_geoip.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 export URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=$CANIHAVE_MAXMIND_KEY&suffix=tar.gz"
+export TAR="GeoLite2-Country.tar.gz"
+export FILE="GeoLite2-Country.mmdb"
 
-echo $URL
-curl --output GeoLite2-Country.tar.gz $URL | tar -xz
+rm ./pkg/location/$FILE
+curl --silent --output $TAR $URL
+tar --extract -z -v --strip-components 1 --file=$TAR $(tar -t -f $TAR | grep mmdb)
+mv ./$FILE ./pkg/location/$FILE
+rm $TAR
